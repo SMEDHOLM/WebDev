008a2dc4ed519b5b79c2296b13e24d603bcab914
SMEDHOLM <smedholm.anton@gmail.com>
2025-11-11 18:29:43 +0200

    database connetction and login added

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import express from  "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";    
+import {validationResult} from 'express-validator'
+import { registerValidation } from "./validations/auth";
 
+
+mongoose.connect("mongodb+srv://admin:admin@cluster0.xknsj14.mongodb.net/?appName=Cluster0").then(( ) => console.log('DB ok'))
+.catch(() => console.log("DB error", err));
 const app = express();
 
-app.get("", (req, res) => {
-    res.send("Hello!");
-});
+app.use(express.json());
+
+
+app.post("/auth/register", registerValidation, (req, res)    => {
+    const errors = validationResult(req) ;
+    if (!errors.isEmpty()) { return res.status(400).json(errors.array())}
+    res.json({
+        success: true,
+    });
+
+    });
 app.listen(4444, (err) => {
     if (err) {
         return console.error(err);
